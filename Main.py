08b4422bad8bbbbd18fa5a005ad0325ddcf4874f
Jasonbhaas <Jasonbhaas@gmail.com>
2017-03-21 15:53:04 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from pprint import pprint
+
+class Game:
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+        self.board = np.matrix
+
+pprint([[0]*3]*3)
+print "hello world"
+
+myMatrix = np.matrix([[' ']*3]*3)
+
+print myMatrix
